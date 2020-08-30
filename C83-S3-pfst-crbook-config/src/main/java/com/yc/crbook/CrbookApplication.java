7694f1bdc68e82b3c5d1d53b6bbd5839bb6f0414
13631 <13631@DESKTOP-3M75H1Q>
2020-08-30 11:43:34 +0800

    修复工程resource确实问题

---
+++ C83-S3-pfst-crbook-config/src/main/java/com/yc/crbook/CrbookApplication.java
@@ -0,0 +1,30 @@
+package com.yc.crbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+ import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+ 
+@SpringBootApplication
+ 
+ 
+//服务降级注解
+@EnableCircuitBreaker
+public class CrbookApplication implements WebMvcConfigurer{
+	 
+
+		public static void main(String[] args) {
+			SpringApplication.run(CrbookApplication.class, args);
+		}
+		
+		
+		@LoadBalanced
+		@Bean //定义RestTemplate Bean
+		public RestTemplate getRestTemplate() {
+			return new RestTemplate();
+		}
+}
